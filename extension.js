be4c13904538c4c14c99f83f9a38cf953f9887ca
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-07-06 19:12:05 -0400

    Function of stopwatch should be done

--- extension.js
+++ extension.js
@@ -38,8 +38,109 @@ function activate(context) {
 		return `
 		<!DOCTYPE html>
 		<html lang="en">
+			<head>
+				<style>
+					* {
+						text-align: center;
+					}
+
+					.grid {
+						display: grid;
+					}
+
+					.left {
+						grid-column: 1;
+					}
+
+					.right {
+						grid-column: 2;
+					}
+				</style>
+			</head>
 			<body>
-				<h1>Hello World</h1>
+				<h1>Timer</h1>
+
+				<div class="grid">
+					<div class="left">
+						<h2>Stopwatch</h2>
+
+						<h3>
+							<span class="digit" id="hr">00</span>
+							<span>:</span>
+							<span class="digit" id="min">00</span>
+							<span>:</span>
+							<span class="digit" id="sec">00</span>
+						</h3>
+
+						<div id="buttons">
+							<button class="btn" id="start">Start</button>
+							<button class="btn" id="stop">Stop</button>
+							<button class="btn" id="reset">Reset</button>
+						</div>
+					</div>
+
+					<div class="right">
+					
+					</div>
+				</div>
+
+				<scirpt>
+					const stopStart = document.getElementById("start");
+					const stopStop = document.getElementById("stop");
+					const stopReset = document.getElementById("reset");
+					const stopTime = {
+						hour: 0,
+						min: 0,
+						sec: 0,
+					}
+					let timer = false;
+
+					function stopWatch() {
+						if (timer) {
+							stopTime.sec = stopTime.sec + 1;
+
+							if (stopTime.sec === 60) {
+								stopTime.sec = 0;
+								stopTime.min = stopTime.min + 1;
+							}
+
+							if (stopTime.min === 60) {
+								stopTime.min = 0;
+								stopTime.hour = stopTime.hour + 1;
+							}
+
+							document.getElementById('hr').innerHTML = stopTime.hour;
+
+							if (stopTime.min < 10) {
+								document.getElementById('min').innerHTML = "0" + str(stopTime.min);
+							} else {
+							  document.getElementById('min').innerHTML = stopTime.min;
+							}
+
+							if (stopTime.sec < 10) {
+								document.getElementById('sec').innerHTML = "0" + str(stopTime.sec);
+							} else {
+							  document.getElementById('sec').innerHTML = stopTime.sec;
+							}
+						}
+					}
+
+					setTimeout(stopWatch, 1000);
+
+					stopStart.addEventListener("click", function () {
+						timer = true;
+					})
+
+					stopStart.addEventListener("click", function () {
+						stopStop = false;
+					})
+
+					stopReset.addEventListener("click", function () {
+						stopTime.hour = 0;
+						stopTime.min = 0;
+						stopTime.sec = 0;
+					})
+				</script>
 			</body>
 		</html>
 		`
